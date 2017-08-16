de039831dd206fff2de1f2bbadd1f91fd06ee869
Bruce Molina <bruce.molina.81@gmail.com>
2017-08-16 10:39:56 +0000

    Done.

--- index.js
+++ index.js
@@ -6,17 +6,14 @@ function nestedTarget(){
   return getFirstSelector('div.target')
 }
 
-
-// parseInt()
 function increaseRankBy(n){
-  const li_items = document.getElementById('app').querySelectorAll('.ranked-list li')
+  const items = document.getElementById("app").querySelectorAll('ul.ranked-list li')
 
-  for (let i = 0; i < li_items.length; i ++){
-
-    li_items[i].innerHTML = (parseInt(li_items[i].innerHTML) + n).toString()
+  for (let i = 0 ; i < items.length ; i++){
+    items[i].innerHTML = (parseInt(items[i].innerHTML) + n)
   }
 }
 
 function deepestChild(){
-  return document.querySelector('#grand-node div div div div')
+  return document.getElementById('grand-node').querySelectorAll('div')[3]
 }
